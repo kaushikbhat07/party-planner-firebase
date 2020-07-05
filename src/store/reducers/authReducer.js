7f577b9a74157ebf58d27e49e6b05861893fd06f
kaushikbhat07 <kaushik.bhatt@learner.manipal.edu>
2020-07-05 16:03:56 +0530

    Authentication - sign in

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,9 +1,24 @@
 const initialState = {
-
+    authError: null
 }
 
 const authReducer = (state = initialState, action) => {
-    return state;
+    switch (action.type) {
+        case 'LOGIN_ERROR':
+            console.log('login error');
+            return {
+                ...state,
+                authError: 'Login Failed'
+            }
+        case 'LOGIN_SUCCESS':
+            console.log('login success');
+            return {
+                ...state,
+                authError: null
+            }
+        default: 
+            return state;       
+    }
 }
 
 export default authReducer;
